1aad39dbdba1fd037782df2844ae160c020f1ae0
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-14 10:21:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -24,3 +24,21 @@ const ctx = canvas.getContext('2d');
 // ctx.rect(canvas.width-110, 10, 100, 100);
 // ctx.rect(10, canvas.height-110, 100, 100);
 // ctx.fill();
+
+// ctx.fillStyle = '#ff0000';
+// ctx.strokeStyle = '#00ff00';
+// ctx.lineWidth = 15;
+// ctx.fillRect(10, 10, 100, 100);
+// ctx.strokeRect(200, 200, 100, 100);
+// ctx.clearRect(50, 50, 20, 20);
+
+// ctx.beginPath();
+// ctx.arc(canvas.width/2, canvas.height/2, 200, 0, Math.PI/2, true);
+// ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(50, 50);
+ctx.lineTo(100, 100);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
+ctx.fill();
