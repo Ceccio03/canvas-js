dda2e9ea7a41f9b8a46d35e65b32f8d97810175f
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-14 11:32:04 +0200

    Update rect.js

--- rect.js
+++ rect.js
@@ -1,21 +1,27 @@
 class Rect {
-    constructor(originX, originY, width, height) {
+    constructor(originX, originY, width, height, color = '#000000') {
         this.originX = originX;
         this.originY = originY;
         this.width = width;
         this.height = height;
+        this.color = color;
     }
 
     draw(ctx) {
+        ctx.strokeStyle = this.color;
         ctx.strokeRect(this.originX, this.originY, this.width, this.height);
     }
 
     static random(canvasWidth, canvasHeight) {
+        const randomRed = Math.floor(256 * Math.random());
+        const randomGreen = Math.floor(256 * Math.random());
+        const randomBlue = Math.floor(256 * Math.random());
+        const rgbString = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
         const randomWidth = 500 * Math.random();
         const randomHeight = 500 * Math.random();
-        const randomX = canvasWidth * Math.random();
-        const randomY = canvasHeight * Math.random();
-        const newRect = new Rect(randomX, randomY, randomWidth, randomHeight);
+        const randomX = (canvasWidth - randomWidth) * Math.random();
+        const randomY = (canvasHeight - randomHeight) * Math.random();
+        const newRect = new Rect(randomX, randomY, randomWidth, randomHeight, rgbString);
 
         return newRect;
     }
