c483f85222af38a6a194de7aa4dce6b9adb04260
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-14 10:36:56 +0200

    commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const r1 = new Rect(50, 80, 300, 200);
+
+r1.draw(ctx);
---
+++ rect.js
@@ -0,0 +1,22 @@
+class Rect {
+    constructor(originX, originY, width, height) {
+        this.originX = originX;
+        this.originY = originY;
+        this.width = width;
+        this.height = height;
+    }
+
+    draw(ctx) {
+        ctx.strokeRect(this.originX, this.originY, this.width, this.height);
+    }
+
+    random(canvasWidth, canvasHeight) {
+        const randomWidth = 500 * Math.random();
+        const randomHeight = 500 * Math.random();
+        const randomX = canvasWidth * Math.random();
+        const randomY = canvasHeight * Math.random();
+        const newRect = new Rect(randomX, randomY, randomWidth, randomHeight);
+
+        return newRect;
+    }
+}
