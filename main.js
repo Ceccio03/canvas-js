c36ea2fbe4f433070b7cbcba5d7f5cff0610ff98
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-14 10:51:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
-for (let i = 0; i < 100; i++) {
-    const r1 = Rect.random(canvas.clientWidth, canvas.height);
+setInterval(createArt, 2000);
 
-    r1.draw(ctx);
+function createArt() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < 100; i++) {
+        const r1 = Rect.random(canvas.width, canvas.height);
+    
+        r1.draw(ctx);
+    }
 }
