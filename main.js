1c14efec2644d71551c9ed88f1480415e89a6d91
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-14 10:02:19 +0200

    commit

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log('canvaaas');
+
+// const canvasCollection = document.getElementsByClassName('my-canvas');
+// const canvasA = canvasCollection[0];
+// console.log(canvasA);
+
+// const canvasCollection2 = document.getElementsByTagName('canvas');
+// const canvasB = canvasCollection2[0];
+// console.log(canvasB);
+
+// const canvasC = document.querySelector('.my-canvas');
+// const canvasD = document.querySelector('#canvas1');
+// const canvasE = document.querySelector('canvas');
+// const canvasCollection3 = document.querySelectorAll('.my-canvas');
+
+const canvas = document.getElementById('canvas1');
+// console.log(canvas.width);
+// console.log(canvas.height);
+
+const ctx = canvas.getContext('2d');
+
+// ctx.rect(10, 10, 100, 100);
+// ctx.rect(canvas.width-110, canvas.height-110, 100, 100);
+// ctx.rect(canvas.width-110, 10, 100, 100);
+// ctx.rect(10, canvas.height-110, 100, 100);
+// ctx.fill();
